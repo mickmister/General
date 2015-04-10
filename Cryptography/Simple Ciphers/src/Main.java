e1b497150ea678c7708235218fc906650f6f24fd
Michael Kochell <kochelmj@rose-hulman.edu>
2015-04-10 16:46:13 -0400

    Updated Cryptography folder structure

---
+++ Cryptography/Simple Ciphers/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main 
+{
+
+	public static void main(String[] args)
+	{
+		new AffineCipher();
+//		new VigenereCipher();
+//		new CopyOfShiftCipher("CONGRATULATIONSYOUBROKETHECIPHER".toUpperCase());
+
+	}
+
+}
